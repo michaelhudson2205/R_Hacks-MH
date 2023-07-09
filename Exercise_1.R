3101398447a3e1a7199bd96617607b75ca7cda6c
Michael Hudson <michaelhudson2205@gmail.com>
2023-07-09 21:16:50 +1000

    Video 7. Exercise 1

---
+++ Exercise_1.R
@@ -0,0 +1,22 @@
+# Video 7: Exercise 1 -----
+lubridate::date()
+lubridate::today()
+
+lubridate::month(lubridate::today())
+
+library(lubridate)
+library(tidyverse)
+
+date()
+today()
+month(today())
+
+d <- '2011-12-12'
+d2 <- ymd(d)
+class(d)
+class(d2)
+
+Sys.Date()
+
+Sys.sleep(20)
+
